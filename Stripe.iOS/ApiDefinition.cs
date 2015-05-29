688d06c389ef1ec1dc932526591ccb1db6b50de3
Renato Todorov <renato.todorov@rocket-internet.com.br>
2015-05-29 18:08:16 -0300

    Implemented the iOS binding itself. ApiDefinition and Enums from Objective Sharpie.

--- Stripe.iOS/ApiDefinition.cs
+++ Stripe.iOS/ApiDefinition.cs
@@ -4,64 +4,380 @@ using UIKit;
 using Foundation;
 using ObjCRuntime;
 using CoreGraphics;
+using PassKit;
 
 namespace Stripe.iOS
 {
-	// The first step to creating a binding is to add your native library ("libNativeLibrary.a")
-	// to the project by right-clicking (or Control-clicking) the folder containing this source
-	// file and clicking "Add files..." and then simply select the native library (or libraries)
-	// that you want to bind.
-	//
-	// When you do that, you'll notice that MonoDevelop generates a code-behind file for each
-	// native library which will contain a [LinkWith] attribute. MonoDevelop auto-detects the
-	// architectures that the native library supports and fills in that information for you,
-	// however, it cannot auto-detect any Frameworks or other system libraries that the
-	// native library may depend on, so you'll need to fill in that information yourself.
-	//
-	// Once you've done that, you're ready to move on to binding the API...
-	//
-	//
-	// Here is where you'd define your API definition for the native Objective-C library.
-	//
-	// For example, to bind the following Objective-C class:
-	//
-	//     @interface Widget : NSObject {
-	//     }
-	//
-	// The C# binding would look like this:
-	//
-	//     [BaseType (typeof (NSObject))]
-	//     interface Widget {
-	//     }
-	//
-	// To bind Objective-C properties, such as:
-	//
-	//     @property (nonatomic, readwrite, assign) CGPoint center;
-	//
-	// You would add a property definition in the C# interface like so:
-	//
-	//     [Export ("center")]
-	//     CGPoint Center { get; set; }
-	//
-	// To bind an Objective-C method, such as:
-	//
-	//     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
-	//
-	// You would add a method definition to the C# interface like so:
-	//
-	//     [Export ("doSomething:atIndex:")]
-	//     void DoSomething (NSObject object, int index);
-	//
-	// Objective-C "constructors" such as:
-	//
-	//     -(id)initWithElmo:(ElmoMuppet *)elmo;
-	//
-	// Can be bound as:
-	//
-	//     [Export ("initWithElmo:")]
-	//     IntPtr Constructor (ElmoMuppet elmo);
-	//
-	// For more information, see http://developer.xamarin.com/guides/ios/advanced_topics/binding_objective-c/
-	//
-}
+	// @interface Stripe : NSObject
+	[BaseType (typeof (NSObject))]
+	interface Stripe {
 
+		// +(void)setDefaultPublishableKey:(NSString *)publishableKey;
+		[Static, Export ("setDefaultPublishableKey:")]
+		void SetDefaultPublishableKey (string publishableKey);
+
+		// +(NSString *)defaultPublishableKey;
+		[Static, Export ("defaultPublishableKey")]
+		string DefaultPublishableKey ();
+	}
+
+	// @interface STPAPIClient : NSObject
+	[BaseType (typeof (NSObject))]
+	interface STPAPIClient {
+
+		// -(instancetype)initWithPublishableKey:(NSString *)publishableKey;
+		[Export ("initWithPublishableKey:")]
+		IntPtr Constructor (string publishableKey);
+
+		// @property (copy, nonatomic) NSString * publishableKey;
+		[Export ("publishableKey")]
+		string PublishableKey { get; set; }
+
+		// @property (nonatomic) NSOperationQueue * operationQueue;
+		[Export ("operationQueue")]
+		NSOperationQueue OperationQueue { get; set; }
+
+		// +(instancetype)sharedClient;
+		[Static, Export ("sharedClient")]
+		STPAPIClient SharedClient ();
+	}
+
+	// @interface BankAccounts (STPAPIClient)
+	[Category]
+	[BaseType (typeof (STPAPIClient))]
+	interface BankAccounts {
+
+		// -(void)createTokenWithBankAccount:(STPBankAccount *)bankAccount completion:(STPCompletionBlock)completion;
+		[Export ("createTokenWithBankAccount:completion:")]
+		void CreateTokenWithBankAccount (STPBankAccount bankAccount, Action<STPToken, NSError> completion);
+	}
+
+	// @interface CreditCards (STPAPIClient)
+	[Category]
+	[BaseType (typeof (STPAPIClient))]
+	interface CreditCards {
+
+		// -(void)createTokenWithCard:(STPCard *)card completion:(STPCompletionBlock)completion;
+		[Export ("createTokenWithCard:completion:")]
+		void CreateTokenWithCard (STPCard card, Action<STPToken, NSError> completion);
+	}
+
+	// @interface PrivateMethods (STPAPIClient)
+//	[Category]
+//	[BaseType (typeof (STPAPIClient))]
+//	interface PrivateMethods {
+//		// -(instancetype)initWithAttributeDictionary:(NSDictionary *)attributeDictionary;
+//		[Export ("initWithAttributeDictionary:")]
+//		IntPtr Constructor (NSDictionary attributeDictionary);
+//
+//		// -(void)createTokenWithData:(NSData *)data completion:(STPCompletionBlock)completion;
+//		[Export ("createTokenWithData:completion:")]
+//		void CreateTokenWithData (NSData data, Action<STPToken, NSError> completion);
+//	}
+
+	// @interface STPBankAccount : NSObject
+	[BaseType (typeof (NSObject))]
+	interface STPBankAccount {
+
+		// @property (copy, nonatomic) NSString * accountNumber;
+		[Export ("accountNumber")]
+		string AccountNumber { get; set; }
+
+		// @property (copy, nonatomic) NSString * routingNumber;
+		[Export ("routingNumber")]
+		string RoutingNumber { get; set; }
+
+		// @property (copy, nonatomic) NSString * country;
+		[Export ("country")]
+		string Country { get; set; }
+
+		// @property (readonly, nonatomic) NSString * bankAccountId;
+		[Export ("bankAccountId")]
+		string BankAccountId { get; }
+
+		// @property (readonly, nonatomic) NSString * last4;
+		[Export ("last4")]
+		string Last4 { get; }
+
+		// @property (readonly, nonatomic) NSString * bankName;
+		[Export ("bankName")]
+		string BankName { get; }
+
+		// @property (readonly, nonatomic) NSString * fingerprint;
+		[Export ("fingerprint")]
+		string Fingerprint { get; }
+
+		// @property (readonly, nonatomic) NSString * currency;
+		[Export ("currency")]
+		string Currency { get; }
+
+		// @property (readonly, nonatomic) BOOL validated;
+		[Export ("validated")]
+		bool Validated { get; }
+
+		// @property (readonly, nonatomic) BOOL disabled;
+		[Export ("disabled")]
+		bool Disabled { get; }
+	}
+
+	// @interface STPCard : NSObject
+	[BaseType (typeof (NSObject))]
+	interface STPCard {
+
+		// @property (copy, nonatomic) NSString * number;
+		[Export ("number")]
+		string Number { get; set; }
+
+		// @property (readonly, nonatomic) NSString * last4;
+		[Export ("last4")]
+		string Last4 { get; }
+
+		// @property (nonatomic) NSUInteger expMonth;
+		[Export ("expMonth")]
+		nuint ExpMonth { get; set; }
+
+		// @property (nonatomic) NSUInteger expYear;
+		[Export ("expYear")]
+		nuint ExpYear { get; set; }
+
+		// @property (copy, nonatomic) NSString * cvc;
+		[Export ("cvc")]
+		string Cvc { get; set; }
+
+		// @property (copy, nonatomic) NSString * name;
+		[Export ("name")]
+		string Name { get; set; }
+
+		// @property (copy, nonatomic) NSString * addressLine1;
+		[Export ("addressLine1")]
+		string AddressLine1 { get; set; }
+
+		// @property (copy, nonatomic) NSString * addressLine2;
+		[Export ("addressLine2")]
+		string AddressLine2 { get; set; }
+
+		// @property (copy, nonatomic) NSString * addressCity;
+		[Export ("addressCity")]
+		string AddressCity { get; set; }
+
+		// @property (copy, nonatomic) NSString * addressState;
+		[Export ("addressState")]
+		string AddressState { get; set; }
+
+		// @property (copy, nonatomic) NSString * addressZip;
+		[Export ("addressZip")]
+		string AddressZip { get; set; }
+
+		// @property (copy, nonatomic) NSString * addressCountry;
+		[Export ("addressCountry")]
+		string AddressCountry { get; set; }
+
+		// @property (readonly, nonatomic) NSString * cardId;
+		[Export ("cardId")]
+		string CardId { get; }
+
+		// @property (readonly, nonatomic) STPCardBrand brand;
+		[Export ("brand")]
+		STPCardBrand Brand { get; }
+
+		// @property (readonly, nonatomic) NSString * type;
+		[Export ("type")]
+		string Type { get; }
+
+		// @property (readonly, nonatomic) STPCardFundingType funding;
+		[Export ("funding")]
+		STPCardFundingType Funding { get; }
+
+		// @property (readonly, nonatomic) NSString * fingerprint;
+		[Export ("fingerprint")]
+		string Fingerprint { get; }
+
+		// @property (readonly, nonatomic) NSString * country;
+		[Export ("country")]
+		string Country { get; }
+
+		// -(BOOL)validateNumber:(id *)ioValue error:(NSError **)outError;
+		[Export ("validateNumber:error:")]
+		bool ValidateNumber (out NSObject ioValue, out NSError outError);
+
+		// -(BOOL)validateCvc:(id *)ioValue error:(NSError **)outError;
+		[Export ("validateCvc:error:")]
+		bool ValidateCvc (out NSObject ioValue, out NSError outError);
+
+		// -(BOOL)validateExpMonth:(id *)ioValue error:(NSError **)outError;
+		[Export ("validateExpMonth:error:")]
+		bool ValidateExpMonth (out NSObject ioValue, out NSError outError);
+
+		// -(BOOL)validateExpYear:(id *)ioValue error:(NSError **)outError;
+		[Export ("validateExpYear:error:")]
+		bool ValidateExpYear (out NSObject ioValue, out NSError outError);
+
+		// -(BOOL)validateCardReturningError:(NSError **)outError;
+		[Export ("validateCardReturningError:")]
+		bool ValidateCardReturningError (out NSError outError);
+	}
+
+	// @interface STPToken : NSObject
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface STPToken {
+
+		// @property (readonly, nonatomic) NSString * tokenId;
+		[Export ("tokenId")]
+		string TokenId { get; }
+
+		// @property (readonly, nonatomic) BOOL livemode;
+		[Export ("livemode")]
+		bool Livemode { get; }
+
+		// @property (readonly, nonatomic) STPCard * card;
+		[Export ("card")]
+		STPCard Card { get; }
+
+		// @property (readonly, nonatomic) STPBankAccount * bankAccount;
+		[Export ("bankAccount")]
+		STPBankAccount BankAccount { get; }
+
+		// @property (readonly, nonatomic) NSDate * created;
+		[Export ("created")]
+		NSDate Created { get; }
+
+		// -(void)postToURL:(NSURL *)url withParams:(NSDictionary *)params completion:(STPCardServerResponseCallback)handler;
+		[Export ("postToURL:withParams:completion:")]
+		void PostToURL (NSUrl url, NSDictionary parameters, Action<NSUrlResponse, NSData, NSError> handler);
+	}
+
+	// @interface STPCheckoutOptions : NSObject <NSCopying>
+	[BaseType (typeof (NSObject))]
+	interface STPCheckoutOptions : INSCopying {
+
+		// -(instancetype)initWithPublishableKey:(NSString *)publishableKey;
+		[Export ("initWithPublishableKey:")]
+		IntPtr Constructor (string publishableKey);
+
+		// @property (copy, nonatomic) NSString * publishableKey;
+		[Export ("publishableKey")]
+		string PublishableKey { get; set; }
+
+		// @property (copy, nonatomic) NSURL * logoURL;
+		[Export ("logoURL", ArgumentSemantic.Copy)]
+		NSUrl LogoURL { get; set; }
+
+		// @property (nonatomic) UIImage * logoImage;
+		[Export ("logoImage")]
+		UIImage LogoImage { get; set; }
+
+		// @property (copy, nonatomic) UIColor * logoColor;
+		[Export ("logoColor", ArgumentSemantic.Copy)]
+		UIColor LogoColor { get; set; }
+
+		// @property (copy, nonatomic) NSString * companyName;
+		[Export ("companyName")]
+		string CompanyName { get; set; }
+
+		// @property (copy, nonatomic) NSString * purchaseDescription;
+		[Export ("purchaseDescription")]
+		string PurchaseDescription { get; set; }
+
+		// @property (nonatomic) NSUInteger purchaseAmount;
+		[Export ("purchaseAmount")]
+		nuint PurchaseAmount { get; set; }
+
+		// @property (copy, nonatomic) NSString * customerEmail;
+		[Export ("customerEmail")]
+		string CustomerEmail { get; set; }
+
+		// @property (copy, nonatomic) NSString * purchaseLabel;
+		[Export ("purchaseLabel")]
+		string PurchaseLabel { get; set; }
+
+		// @property (copy, nonatomic) NSString * purchaseCurrency;
+		[Export ("purchaseCurrency")]
+		string PurchaseCurrency { get; set; }
+
+		// @property (copy, nonatomic) NSNumber * enableRememberMe;
+		[Export ("enableRememberMe", ArgumentSemantic.Copy)]
+		NSNumber EnableRememberMe { get; set; }
+
+		// @property (copy, nonatomic) NSNumber * enablePostalCode;
+		[Export ("enablePostalCode", ArgumentSemantic.Copy)]
+		NSNumber EnablePostalCode { get; set; }
+
+		// @property (copy, nonatomic) NSNumber * requireBillingAddress;
+		[Export ("requireBillingAddress", ArgumentSemantic.Copy)]
+		NSNumber RequireBillingAddress { get; set; }
+
+		// -(NSString *)stringifiedJSONRepresentation;
+		[Export ("stringifiedJSONRepresentation")]
+		string StringifiedJSONRepresentation ();
+	}
+
+	// @interface STPCheckoutViewController : UINavigationController
+	[BaseType (typeof (UINavigationController))]
+	interface STPCheckoutViewController {
+
+		// -(instancetype)initWithOptions:(STPCheckoutOptions *)options;
+		[Export ("initWithOptions:")]
+		IntPtr Constructor (STPCheckoutOptions options);
+
+		// @property (readonly, copy, nonatomic) STPCheckoutOptions * options;
+		[Export ("options", ArgumentSemantic.Copy)]
+		STPCheckoutOptions Options { get; }
+
+		// @property (nonatomic, weak) id<STPCheckoutViewControllerDelegate> checkoutDelegate;
+		[Export ("checkoutDelegate", ArgumentSemantic.Weak)]
+		[NullAllowed]
+		NSObject WeakCheckoutDelegate { get; set; }
+
+		// @property (nonatomic, weak) id<STPCheckoutViewControllerDelegate> checkoutDelegate;
+		[Wrap ("WeakCheckoutDelegate")]
+		STPCheckoutViewControllerDelegate CheckoutDelegate { get; set; }
+	}
+
+	// @protocol STPCheckoutViewControllerDelegate <NSObject>
+	[Protocol, Model]
+	[BaseType (typeof (NSObject))]
+	interface STPCheckoutViewControllerDelegate {
+
+		// @required -(void)checkoutController:(STPCheckoutViewController *)controller didFinishWithStatus:(STPPaymentStatus)status error:(NSError *)error;
+		[Export ("checkoutController:didFinishWithStatus:error:")]
+		[Abstract]
+		void DidFinishWithStatus (STPCheckoutViewController controller, STPPaymentStatus status, NSError error);
+
+		// @required -(void)checkoutController:(STPCheckoutViewController *)controller didCreateToken:(STPToken *)token completion:(STPTokenSubmissionHandler)completion;
+		[Export ("checkoutController:didCreateToken:completion:")]
+		[Abstract]
+		void DidCreateToken (STPCheckoutViewController controller, STPToken token, Action<STPBackendChargeResult, NSError> completion);
+	}
+
+	// @interface ApplePay (STPAPIClient)
+	[Category]
+	[BaseType (typeof (STPAPIClient))]
+	interface ApplePay {
+
+		// -(void)createTokenWithPayment:(PKPayment *)payment completion:(STPCompletionBlock)completion;
+		[Export ("createTokenWithPayment:completion:")]
+		void CreateTokenWithPayment (PKPayment payment, Action<STPToken, NSError> completion);
+
+		// +(NSData *)formEncodedDataForPayment:(PKPayment *)payment;
+		[Static, Export ("formEncodedDataForPayment:")]
+		NSData FormEncodedDataForPayment (PKPayment payment);
+
+		// +(BOOL)canSubmitPaymentRequest:(PKPaymentRequest *)paymentRequest;
+		[Static, Export ("canSubmitPaymentRequest:")]
+		bool CanSubmitPaymentRequest (PKPaymentRequest paymentRequest);
+
+		// +(PKPaymentRequest *)paymentRequestWithMerchantIdentifier:(NSString *)merchantIdentifier;
+		[Static, Export ("paymentRequestWithMerchantIdentifier:")]
+		PKPaymentRequest PaymentRequestWithMerchantIdentifier (string merchantIdentifier);
+
+		// +(void)createTokenWithPayment:(PKPayment *)payment completion:(STPCompletionBlock)handler;
+//		[Availability (Deprecated = Platform.iOS | Platform.Mac)]
+//		[Static, Export ("createTokenWithPayment:completion:")]
+//		void CreateTokenWithPayment (PKPayment payment, Action<STPToken, NSError> handler);
+
+		// +(void)createTokenWithPayment:(PKPayment *)payment operationQueue:(NSOperationQueue *)queue completion:(STPCompletionBlock)handler;
+		[Static, Export ("createTokenWithPayment:operationQueue:completion:")]
+		void CreateTokenWithPayment (PKPayment payment, NSOperationQueue queue, Action<STPToken, NSError> handler);
+	}
+}
--- Stripe.iOS/StructsAndEnums.cs
+++ Stripe.iOS/StructsAndEnums.cs
@@ -1,6 +1,45 @@
 ﻿using System;
+using ObjCRuntime;
 
 namespace Stripe.iOS
 {
-}
+	public enum STPErrorCode : int /* nint */ {
+		STPConnectionError = 40,
+		STPInvalidRequestError = 50,
+		STPAPIError = 60,
+		STPCardError = 70,
+		STPCheckoutError = 80
+	}
 
+	[Native]
+	public enum STPCardFundingType : long /* nint */ {
+		Debit,
+		Credit,
+		Prepaid,
+		Other
+	}
+
+	[Native]
+	public enum STPCardBrand : long /* nint */ {
+		Visa,
+		Amex,
+		MasterCard,
+		Discover,
+		JCB,
+		DinersClub,
+		Unknown
+	}
+
+	[Native]
+	public enum STPPaymentStatus : long /* nint */ {
+		Success,
+		Error,
+		UserCancelled
+	}
+
+	[Native]
+	public enum STPBackendChargeResult : long /* nint */ {
+		Success,
+		Failure
+	}
+}
